be1f04f154c7458ef18be60ee0bceff62bb42bf9
Dio <cdebon97@outlook.com>
2022-01-26 13:40:21 -0600

    Initial Commit

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // your code here
+
+var button = document.getElementById('submit');
+
+button.addEventListener('click', function () {
+  var name = document.getElementById('name').value;
+  var message = document.getElementById('message').value;
+
+  var newDiv = document.createElement('div');
+  var userMessage = document.createElement('p');
+  var userName = document.createElement('p');
+  
+  userMessage.append(message);
+  userName.append('Posted By: ' + name);
+
+  newDiv.appendChild(userMessage);
+  newDiv.appendChild(userName);
+
+  var posts = document.getElementsByClassName('posts')[0];
+
+  posts.append(newDiv);
+})
